7143829bd24cd139e017251204a86e1fd81cce38
afvial <afvial@gmail.com>
2024-02-20 20:53:34 +0100

    upload

--- str_num_alpha.py
+++ str_num_alpha.py
@@ -1,2 +1,10 @@
 # How to Check if Strings Represent Numbers or Alphanumeric Characters in Python
 
+api_data = '5'
+greeting = 'Hi there'
+
+# print(api_data.isalpha())
+print(greeting.isalpha())
+
+# print(api_data.isnumeric())
+# print(greeting.isnumeric())
